ab1f4d8c47b3fb4c43178678ff1e0373c1248181
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-01-21 15:33:10 +0530

    Added Views for home,posts and users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,9 @@
 // Controllers are the actions/group of actions taken for the routes.
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for codeial! </h1>');
+    // return res.end('<h1>Express is up for codeial! </h1>');
+    return res.render('home', //this will directly look up in the views folder 
+        {
+            title : "Home"
+        }
+    ); //called using home views. 
 }
--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -1,3 +1,6 @@
 module.exports.posts = function(req,res){
-     res.end('<h1>Rendering output from Posts Controller. </h1>');
+     // res.end('<h1>Rendering output from Posts Controller. </h1>');
+     res.render('posts',{
+          title:"Posts"
+     });
 }
