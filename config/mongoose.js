e15123fa88659815534af2e4bd03c29ddfaa8f28
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-01-31 23:05:17 +0530

    Set-up Mongo DB

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+// Acquiring mongoose from the library.
+const mongoose = require('mongoose');
+// Define the connection environment for mongoose
+mongoose.connect('mongodb://localhost/codeial-development');
+// establishing the DB connection
+const db = mongoose.connection;
+// Checking whether the connection is established or not.
+db.on('error',console.error.bind(console,"Error connecting to Mongo DB!"));
+db.once('open',function(){
+    console.log("Connected to Database :: Mongo DB!");
+});
+module.exports = db; //to make it usable from our app outside this file.
--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 const express = require('express');
 const app = express();//Creates an Express application. The express() function is a top-level function exported by the express module. 
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts'); //acquire layouts
+const db = require('./config/mongoose'); //acquire the defined mongoose connection
+
+// tell the app to use static files in assets folder.
+app.use(express.static('./assets'));
+
+// tell the app to  use the layouts before the views to render pages accordingly as they're the wrappers for every view. 
+app.use(expressLayouts);
+
+// tell the app to extract styles & scripts from sub-pages and put into the layout.
+app.set('layout extractStyles',true); //for css files
+app.set('layout extractScripts',true); //for js files
+
+//tell the app to use EJS as view-engine.
+app.set('view engine','ejs');
+app.set('views','./views');
 
 //use express router.
 app.use('/',require('./routes')); //by default it takes index.js from routes. 
@@ -14,7 +30,3 @@ app.listen(port,function(err){
     }
     console.log(`Server is up and running on Port: ${port}`);
 });
-
-//tell the app to use EJS as view-engine.
-app.set('view engine','ejs');
-app.set('views','./views');
