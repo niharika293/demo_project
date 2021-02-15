dc11bef27bb4cfae01f867d9b40a3242ee35ed5f
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-02-15 13:27:26 +0530

    Delete an authorized post

--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 module.exports.create = function(req,res){
      //Create a post
@@ -13,4 +14,21 @@ module.exports.create = function(req,res){
           }
           return res.redirect('back');
      });
+}
+
+module.exports.destroy = function(req,res){
+     // Find whether the post exists in the DB or not.
+     Post.findById(req.params.id,function(err,post){
+          //This will check if the post id matches with the user id who's logged in, only then the post will be deleted.
+          if(post.user == req.user.id){ //Initially post.user is going to be a string ID 
+               // .id means converting the object id into string.
+               post.remove();
+               Comment.deleteMany({post:req.params.id},function(err){
+                    return res.redirect('back');
+               });
+          }
+          else{
+               return res.redirect('back');
+          }
+     })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postsController = require('../controllers/posts_contoller');
 // Putting a check at the URL level to limit accessibility only to the authenticated users 
 router.post('/create',passport.checkAuthentication,postsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
 module.exports = router;
 
