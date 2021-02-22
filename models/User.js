7c28ff9cd2dc8c8c2681d143e32a9d74089fc3ab
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-02-22 20:31:37 +0530

    Use multer and configure for the user avatar

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 // Acquire mongoose library
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 // Create a schema
 const userSchema = new mongoose.Schema({
     email:{
@@ -14,10 +17,23 @@ const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:{
+        type : String
     }
 },{
     timestamps:true // this creates the fields - createdAt, modifiedAt
 }); 
+
+// configuring multer for using avatar
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  })
 // tell the mongoose that user is a model
 const User = mongoose.model('User',userSchema);
 module.exports = User;
