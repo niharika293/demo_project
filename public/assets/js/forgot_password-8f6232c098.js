d37284f446aca872c07cfb6c7b062071bf7eb64a
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-04-03 17:44:05 +0530

    Clean-up ignored files!!

--- public/assets/js/forgot_password-8f6232c098.js
+++
@@ -1,1 +0,0 @@
-console.log("Forgot password script loaded successfully!");var email=$("#inp_uname"),pass1=$("#inp_pw"),pass2=$("#inp_cnf_pw"),btn_submit=$("#btn_submit");function displayAlert(){""!=email.val()&&""!=pass1.val()&&""!=pass2.val()&&alert("Congratulations!! Your password has been sucessfully changed now! Login with your new credentials to get started!")}
